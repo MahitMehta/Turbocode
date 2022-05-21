4a381b86c2ba867038cedc0b2e59a546c20bb950
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-1189.local>
2022-05-21 10:32:00 -0400

    Add Dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,8 +32,6 @@ export default defineNuxtConfig({
     buildModules: [
         // https://go.nuxtjs.dev/tailwindcss
         '@nuxtjs/tailwindcss',
-        '@nuxt-hero-icons/solid/nuxt',
-        '@nuxt-hero-icons/outline/nuxt',
     ],
     plugins: [
         "~/plugins/apolloClient.ts",
