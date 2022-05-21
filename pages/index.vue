efd1a8f461d62b715825bfab137339d5f26981a9
Yauk-An-Ant <avc0824@gmail.com>
2022-05-21 13:17:47 -0400

    finished login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="container h-screen">
         <div class="heading">
             <p>Morb.it</p>
         </div>
@@ -9,12 +9,12 @@
         <div class="align-items-center w-[12%] h-[30%] buttonline">
             <div class="sign">
                 <NuxtLink to="pages/signup">
-                    <button type="button" class="button w-[75%] h-[100%] text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-[50px] mr-2 mb-2 ">Sign Up</button>
+                    <button type="button" class="button w-[75%] h-[100%] text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Sign Up</button>
                 </NuxtLink>
             </div>
             <div class="log w-[100%]">
                 <NuxtLink to="pages/login">
-                    <button type="button" class="button w-[100%] h-[100%] text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Log In</button>        
+                    <button type="button" class=" button w-[100%] h-[100%] text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-[50px] mr-2 mb-2 ">Log In</button>        
                 </NuxtLink>
             </div>
         </div>
@@ -28,10 +28,11 @@
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    background: #151515;
 }
 .heading {
     color: #0A758F;
-    margin-top: 15%;
+    margin-top: 10%;
     font-size: 75px;
     margin-bottom: 20px;
     text-decoration: none;
@@ -40,6 +41,7 @@
     color: #971F1F;
     font-size: 20px;
     text-decoration: none;
+    margin-bottom: 35px;
 }
 .button {
     font-family: 'Courier New';
@@ -49,7 +51,7 @@
     margin-right: 40%;
 }
 .log {
-    margin-top: -52px;
+    margin-top: -100px;
     margin-left: 65%;
 }
 .buttonline {
--- pages/login.vue
+++ pages/login.vue
@@ -1,19 +1,32 @@
 <template>
-    <div class="flex justify-center">
-        <form class="w-[50%] h-[200%] mt-[14.888%]">
-  <div class="mb-6">
-    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#0A758F] focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@domain.com" required>
-  </div>
-  <div class="mb-6">
-    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
-    <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-  </div>
-  <div class="flex items-start mb-6">
-    <div class="flex items-center h-5">
-    </div>
-  </div>
-  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-</form>
-    </div>
+    <div class="flex justify-center h-screen container w-screen">
+        <div class="p-4 w-[25%] bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <form class="space-y-6 w-[100%]" action="#">
+        <h5 class="text-xl font-medium text-gray-900 dark:text-white">Log In</h5>
+        <div>
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
+            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required>
+        </div>
+        <div>
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
+            <input type="password" name="password" id="password" placeholder="********" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white mb-2" required>
+        </div>
+        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+            Not registered? <NuxtLink to="pages/signup" class="text-blue-700 hover:underline dark:text-blue-500">Create account</NuxtLink>
+        </div>
+    </form>
+</div>
+</div>
 </template>
+
+<style>
+.container {
+    font-family: 'Courier New';
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: #151515;
+}
+</style>
